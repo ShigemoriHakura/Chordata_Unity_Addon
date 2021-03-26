5b7ec18f97cc942c034c65c3e70bf5f847e2f81b
ShigemoriHakura <62388797+ShigemoriHakura@users.noreply.github.com>
2021-03-27 04:04:07 +0800

    clone from https://github.com/thomasfredericks/UnityOSC
    https://github.com/thomasfredericks/UnityOSC

---
+++ Assets/ReceivePosition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceivePosition : MonoBehaviour {
+    
+   	public OSC osc;
+
+
+	// Use this for initialization
+	void Start () {
+	   osc.SetAddressHandler("/%/kc-CH_4-2", OnReceiveXYZ );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnReceiveXYZ(OscMessage message){
+        Debug.Log(message.ToString());
+        float w = message.GetFloat(0);
+        float x = message.GetFloat(1);
+		float y = message.GetFloat(2);
+        float z = message.GetFloat(3);
+
+        transform.localRotation = new Quaternion(x, -z, y, w);
+	}
+
+}
